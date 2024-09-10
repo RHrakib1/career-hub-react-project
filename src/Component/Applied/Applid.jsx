0fa3e5eda462f1ed7479b3d3f4c49c0f7a77676e
RHrakib1 <rakibhasanmd457@gmail.com>
2024-09-10 22:43:55 +0600

    commit to the filter

--- src/Component/Applied/Applid.jsx
+++ src/Component/Applied/Applid.jsx
@@ -9,6 +9,21 @@ const Applid = () => {
 
 
     const [allpy, setallpy] = useState([])
+    const [displayjob, setdisplayjob] = useState([])
+    const hendlejobFelter = filter => {
+        if (filter === "All") {
+            setdisplayjob(allpy)
+        }
+        else if (filter === "Remote") {
+            const filteris = allpy.filter(job => job.remote_or_onsite === "Remote")
+            setdisplayjob(filteris)
+        }
+        else if (filter === "Onsite") {
+            const onsitejob = allpy.filter(job => job.remote_or_onsite === "Onsite")
+            setdisplayjob(onsitejob)
+
+        }
+    }
     useEffect(() => {
         const storgjobid = getstorJobApplication()
         if (job.length > 0) {
@@ -21,14 +36,23 @@ const Applid = () => {
                 }
             }
             setallpy(jobApplid)
+            setdisplayjob(jobApplid)
         }
     }, [job, setallpy])
     return (
         <div>
             <h2 className="text-3xl font-semibold text-center">Applied Job: {allpy.length}</h2>
+            <details className="dropdown">
+                <summary className="btn m-1">Filter on jobs</summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li onClick={() => hendlejobFelter('All')}><a>All</a></li>
+                    <li onClick={() => hendlejobFelter('Remote')}><a>Remote</a></li>
+                    <li onClick={() => hendlejobFelter('Onsite')}><a>Onside</a></li>
+                </ul>
+            </details>
             <div className="lg:grid grid-cols-1 gap-3 mb-10">
                 {
-                    allpy.map((copyapply, idx) => <Apply key={idx} apply={copyapply}></Apply>)
+                    displayjob.map((copyapply, idx) => <Apply key={idx} apply={copyapply}></Apply>)
                 }
             </div>
 
